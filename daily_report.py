70be0d9ee7a3ad2e24e230e9ee18507aebc9c9d3
Nicolas Carreño <nicolascarreno@192.168.1.15>
2026-08-11 10:55:55 -0300

    Fix reporte diario (faltaba usuario del pooler) y falta httpx para Apollo

--- daily_report.py
+++ daily_report.py
@@ -16,8 +16,11 @@ ART = ZoneInfo("America/Argentina/Buenos_Aires")
 
 def db_conn():
     return psycopg2.connect(
-        host=os.environ["SUPABASE_DB_HOST"], port=5432, dbname="postgres",
-        user="postgres", password=os.environ["SUPABASE_DB_PASS"], sslmode="require",
+        host=os.environ["SUPABASE_DB_HOST"],
+        port=int(os.environ.get("SUPABASE_DB_PORT", 5432)),
+        dbname="postgres",
+        user=os.environ.get("SUPABASE_DB_USER", "postgres"),
+        password=os.environ["SUPABASE_DB_PASS"], sslmode="require",
         connect_timeout=10
     )
 
